43e8d9e2f826fc0bfe0c0fa183685c6215b39b57
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-12-07 17:52:01 +0100

    Lectura de xml

--- uaserver.py
+++ uaserver.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-''' USER AGENT CLIENT '''
+''' USER AGENT SERVER'''
 
 import socket
 import sys
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+
 
 ''' READING AND EXTRACTION OF XML DATA'''
 
@@ -55,3 +56,63 @@ ACCOUNT = data[0]['account']
 print("Esto es account: ", ACCOUNT)
 USERNAME = ACCOUNT['username']
 print("Esto es username:", USERNAME)
+
+
+class EchoHandler(socketserver.DatagramRequestHandler):
+
+def handle(self):
+        """Escribe dirección y puerto del cliente (de tupla client_address)."""
+        while 1:
+            # Leyendo línea a línea lo que nos envía el cliente
+            text = self.rfile.read()
+            line = self.rfile.read()
+            print("El cliente nos manda " + text.decode('utf-8'))
+            LINE = text.decode('utf-8')
+            Words_LINES = LINE.split()
+            REQUEST = Words_LINES[0]
+            print("La peticion es: ", REQUEST)
+            print("Listening...")
+
+            if REQUEST == 'INVITE':
+                answer100 = b"SIP/2.0 100 Trying\r\n\r\n"
+                answer180 = b"SIP/2.0 180 Ring\r\n\r\n"
+                answer200 = b"SIP/2.0 200 OK\r\n\r\n"
+                ANSWER = answer100 + answer180 + answer200
+                self.wfile.write(ANSWER)
+
+            elif REQUEST == 'ACK':
+                SONG = (sys.argv[3])
+                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + self.SONG
+                print ('Vamos a ejecutar', aEjecutar)
+                os.system(aEjecutar)
+            elif REQUEST == 'BYE':
+                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+
+            # Si no hay más líneas salimos del bucle infinito
+            if not line:
+                break
+
+if __name__ == "__main__":
+    # Creamos servidor de eco y escuchamos
+    serv = socketserver.UDPServer((sys.argv[1], int(sys.argv[2])), EchoHandler)
+    print("Lanzando servidor UDP de eco...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
