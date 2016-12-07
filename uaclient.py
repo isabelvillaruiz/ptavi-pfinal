68c4f94228d9091fac54566cd44ec15cdded027e
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-12-07 17:51:33 +0100

    Creacion de lineas y comunicacion con proxy

--- uaclient.py
+++ uaclient.py
@@ -18,11 +18,12 @@ REQUEST = sys.argv[2]
 if REQUEST == 'REGISTER':
     EXPIRES = sys.argv[3]
 elif REQUEST == 'INVITE':
-    USUARIO == sys.argv[3]
+    USUARIO = sys.argv[3]
 elif REQUEST == 'BYE':
-    USUARIO == sys.arvg[3]
+    USUARIO = sys.argv[3]
 
-class SmallSMILHandler(ContentHandler):
+
+class Handler(ContentHandler):
 
     def __init__(self):
 
@@ -48,7 +49,7 @@ class SmallSMILHandler(ContentHandler):
 
 
 parser = make_parser()
-cHandler = SmallSMILHandler()
+cHandler = Handler()
 parser.setContentHandler(cHandler)
 parser.parse(open(XML_DATA))
 data = cHandler.get_tags()
@@ -61,11 +62,36 @@ print(data)
 
 ACCOUNT = data[0]['account']
 print("Esto es account: ", ACCOUNT)
+
 USERNAME = ACCOUNT['username']
 print("Esto es username:", USERNAME)
+
 UASERVER_PORT = data[1]['uaserver']['puerto']
 print("Esto es el puerto de escucha del UAServer:", UASERVER_PORT)
+
 UASERVER_IP = data[1]['uaserver']['ip']
+print("Esto es la direccion IP del UASERVER: ", UASERVER_IP)
+
+RTP_PORT = data[2]['rtpaudio']['puerto']
+print("Esto es el puerdo de escuha de audio RTP: ", RTP_PORT)
+
+PROXY_PORT = data[3]['regproxy']['puerto']
+print("Esto es el puerto del proxy: ", PROXY_PORT)
+
+PROXY_IP = data[3]['regproxy']['ip']
+print("Esto es el puerto del proxy: ", PROXY_IP)
+
+
+
+
+
+'''SOCKET'''
+# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((PROXY_IP,int(PROXY_PORT)))
+
+
 
 
 '''REQUESTS and their lines '''
@@ -77,8 +103,61 @@ if REQUEST == "REGISTER":
     LINE = "REGISTER" + SIP_LINE + EXPIRES_LINE
     print("Esta es la linea que voy a enviar si es REGISTER: ")
     print(LINE)
+    print("Enviando: " + "\r\n" + LINE)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+    print(data.decode('utf-8'))
 elif REQUEST == "INVITE":
-     
+    SIP_INFO = USERNAME
+    SIP_LINE = " sip:" + SIP_INFO + " SIP/2.0\r\n"
+    SDP_LINE_CT = "Content-Type: application/sdp" + "\r\n"
+    print()
+    v = "v=0"
+    o = "o=" + USERNAME + " " + UASERVER_IP
+    s = "s=SesionGhibli"
+    t = "t=0"
+    m = "m=audio " + RTP_PORT + " RTP"
+    SDP_LINE_1 = "\r\n" + v + "\r\n" + o + "\r\n" + s + "\r\n"
+    SDP_LINE_2 = t + "\r\n" + m + "\r\n"
+    SDP_LINE = SDP_LINE_CT + SDP_LINE_1 + SDP_LINE_2
+    #print("ESTA ES UNA PRUEBA DE LA LINEA SDP", SDP_LINE)
+    LINE = "INVITE" + SIP_LINE + SDP_LINE
+    print("Esta es la linea que voy a enviar si es INVITE: ")
+    print(LINE)
+    print("Enviando: " + LINE)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+    print(data.decode('utf-8'))
+elif REQUEST == "BYE":
+    SIP_INFO = USERNAME
+    LINE = "BYE" + " sip:" + SIP_INFO + " SIP/2.0\r\n"
+    print("Enviando: " + LINE)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+    print(data.decode('utf-8'))
+
+'''
+print("Enviando: " + LINE)
+my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+data = my_socket.recv(1024)
+'''
+
+# Cerramos todo
+my_socket.close()
+print("Fin.")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
