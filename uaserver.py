088b03c4681c858fea93a15d7ee8d4bccff83575
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-12-11 19:12:21 +0100

    RECIBE INVITE Y RESPONDE AL PROXY CON SU SDP PARA EL CLIENTE

--- uaserver.py
+++ uaserver.py
@@ -10,13 +10,17 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
+
+
 ''' READING AND EXTRACTION OF XML DATA'''
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python uaserver.py config")
 
 #FIRST PARAMETER : XML FILE 
 XML_DATA = sys.argv[1]
 
-if len(sys.argv) != 2:
-    sys.exit("Usage: python uaserver.py config")
+
 
 
 class SmallSMILHandler(ContentHandler):
@@ -64,6 +68,7 @@ UASERVER_PORT = data[1]['uaserver']['puerto']
 print("Esto es el puerto de escucha del UAServer:", UASERVER_PORT)
 UASERVER_IP = data[1]['uaserver']['ip']
 print("Esto es la direccion IP del UASERVER: ", UASERVER_IP)
+RTP_PORT = data[2]['rtpaudio']['puerto']
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -74,21 +79,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 # Leyendo línea a línea lo que nos envía el cliente
                 text = self.rfile.read()
                 line = self.rfile.read()
-                print("El cliente nos manda " + text.decode('utf-8'))
+                print("El proxy nos manda del cliente " + "\r\n" + text.decode('utf-8'))
                 LINE = text.decode('utf-8')
-
+                METHODS = ['INVITE', 'ACK', 'BYE']
                 Words_LINES = LINE.split()
                 print("Esta es la linea que me envia el proxy", Words_LINES)                
                 REQUEST = Words_LINES[0]
                 print("FUNCIONA")
+
+                #Ahora que lo pienso es estupido porq el 405 lo detectara el prxy antes XD
+                if not REQUEST in METHODS:
+                    LINE_405 = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
+                    self.wfile.write(LINE_405)           
+
                 if REQUEST == 'INVITE': 
-                    answer100 = b"SIP/2.0 100 Trying\r\n\r\n"
-                    answer180 = b"SIP/2.0 180 Ring\r\n\r\n"
-                    answer200 = b"SIP/2.0 200 OK\r\n\r\n"
-                    ANSWER = answer100 + answer180 + answer200
-                    self.wfile.write(ANSWER)
+                    answer = "SIP/2.0 100 Trying\r\n\r\n"
+                    answer += "SIP/2.0 180 Ring\r\n\r\n"
+                    answer += "SIP/2.0 200 OK\r\n\r\n"
+                    answer += "Content-Type: application/sdp\r\n\r\n"
+                    answer += "v=0\r\n" + "o=" + USERNAME + " "
+                    answer += UASERVER_IP + " \r\n" + "s=SesionGhibli\r\n"
+                    answer += "t=0\r\n" + "m=audio " + RTP_PORT + " RTP\r\n\r\n"
+                    self.wfile.write(bytes(answer, 'utf-8'))
                 elif REQUEST == 'BYE':
-                    print("BATMAN!")
+                    print("HOWL!")
 
                 # Si no hay más líneas salimos del bucle infinito
                 if not line:
@@ -102,7 +116,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer((UASERVER_IP, int(UASERVER_PORT)), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    print("Listening...")
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
